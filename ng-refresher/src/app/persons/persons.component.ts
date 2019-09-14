fc180507ddcf0c999f055e447fc7b82dc1cc8be8
mcartagena <marcelocartagenaorellana@gmail.com>
2019-09-14 15:26:41 -0300

    Se agrega proyecto para refrescar conocimiento de angular

---
+++ ng-refresher/src/app/persons/persons.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+  styleUrls: ['./persons.component.css']
+})
+export class PersonsComponent implements OnInit {
+  @Input() personsList:string[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
