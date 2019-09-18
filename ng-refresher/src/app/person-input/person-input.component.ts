4affe19cecd5e53b7635b2084108f759050ca309
mcartagena <marcelocartagenaorellana@gmail.com>
2019-09-18 14:02:02 -0300

    Se agrega input y local reference en Angular html y component

---
+++ ng-refresher/src/app/person-input/person-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+export class PersonInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onCreatePerson(personName: string){
+    console.log('Created Person: ' + personName);
+  }
+}
