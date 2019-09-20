b40e06aa7eaa5f844966b1bd5eb7d527c98905a8
mcartagena <marcelocartagenaorellana@gmail.com>
2019-09-20 07:44:22 -0300

    removing and observables

--- ng-refresher/src/app/persons/persons.component.ts
+++ ng-refresher/src/app/persons/persons.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
 
   personsList: string[];
   //private personService: PersonsService;
+  private personListSubs: Subscription;
 
   constructor(private prsService: PersonsService) {
     //this.personsList = prsService.persons;
@@ -18,6 +20,16 @@ export class PersonsComponent implements OnInit {
 
   ngOnInit() {
     this.personsList = this.prsService.persons;
+    this.personListSubs = this.prsService.personChanged.subscribe(persons => {
+      this.personsList = persons;
+    });
   }
 
+  ngOnDestroy(){
+    this.personListSubs.unsubscribe();
+  }
+
+  onRemovePerson(name: string){
+    this.prsService.removePerson(name);
+  }
 }
--- ng-refresher/src/app/persons/persons.service.ts
+++ ng-refresher/src/app/persons/persons.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonsService {
-
+  personChanged = new Subject<string[]>();
   public persons: string[] = ['Max','Manuel','Anna'];
 
   constructor() { }
 
   addPerson(name: string){
     this.persons.push(name);
-    console.log(this.persons);
+    this.personChanged.next(this.persons);
+  }
+
+  removePerson(name: string){
+    this.persons = this.persons.filter(person => {
+      return person!=name;
+    });
+    this.personChanged.next(this.persons);
   }
 }
